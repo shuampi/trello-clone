607f8ed729af353d70d09f460f327fd428caaab5
shuamp <castel.davila@gmail.com>
2022-09-15 13:56:45 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { AppContainer, ColumnContainer, ColumnTitle, CardContainer } from './styles';
 
-function App() {
+
+export function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <AppContainer>
+      <ColumnContainer>
+        <ColumnTitle>TODO:</ColumnTitle>
+        <CardContainer>FirstItem</CardContainer>
+        <CardContainer>SecondItem</CardContainer>
+        <CardContainer>ThirdItem</CardContainer>
+      </ColumnContainer>
+   </AppContainer>
   );
 }
 
-export default App;
+

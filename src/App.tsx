6faf454188a25ee9d59fa0b4b700f18da25ad3f2
shuamp <castel.davila@gmail.com>
2022-09-22 13:24:18 +0100

    column component updated with addnewitem component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import {Column} from './Column';
 import { AppContainer } from './styles';
+import { AddNewItem } from './AddNewItem';
 
 
 export function App() {
   return (
    <AppContainer>
       <Column text="TODO:"/>
+      <AddNewItem toggleButtonText="+ Add another list"
+      onAdd={console.log}
+      />
    </AppContainer>
   );
 }
--- src/Column.tsx
+++ src/Column.tsx
@@ -1,6 +1,7 @@
 
 import { ColumnContainer, ColumnTitle} from "./styles";
 import {Card} from './Card'
+import { AddNewItem } from "./AddNewItem";
 type ColumnProps = {
     text: string
 }
@@ -12,6 +13,7 @@ return (
         <Card text="Generate App Scaffold"/>
         <Card text="Learn TypeScript"/>
         <Card text="Begin to use static typing"/>
+        <AddNewItem toggleButtonText="+ Add another card" onAdd={console.log} dark />
     </ColumnContainer>
 )
 }
